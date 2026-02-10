3975ecacd3c3294fd06c719ba7bcc084d5b3b6d3
Insanity <chaininsanity@gmail.com>
2026-02-10 22:26:05 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+{
+  "linesOfCode": "~1,200",
+  "fileSize": "~85 KB",
+  "loadTime": "< 1 second",
+  "dependencies": "0",
+  "buildTools": "None",
+  "frameworks": "None",
+  "languages": ["HTML5", "CSS3", "JavaScript ES6+"],
+  "accessibilityScore": "95/100",
+  "performanceScore": "98/100",
+  "bestPractices": "92/100"
+}
